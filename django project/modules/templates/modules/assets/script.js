59a2863cc1443868aed87903c38f2e7f02d9af7c
Sizwe <smngcemu@gmail.com>
2025-03-05 11:32:26 +0200

    add the JS file

---
+++ django project/modules/templates/modules/assets/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementsByClassName("modules-panel")[0];
+  const modulesSelect = document.getElementById("modules-select");
+  let timeoutId = null;
+
+  if (dropdown && modulesSelect) {
+    modulesSelect.addEventListener("mouseover", function () {
+      dropdown.style.display = "flex";
+    });
+    dropdown.addEventListener("mouseout", function () {
+      timeoutId = setTimeout(() => {
+        dropdown.style.display = "none";
+      }, 5000);
+    });
+  }
+});
