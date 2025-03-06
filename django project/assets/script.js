0504aa809faad72cdd0dc03fa6c3476ffc35e522
Sizwe <smngcemu@gmail.com>
2025-03-06 08:31:20 +0200

    Minor changes days 3

--- django project/assets/script.js
+++ django project/assets/script.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const dropdown = document.getElementsByClassName("modules-panel")[0];
-  const modulesSelect = document.getElementById("modules-select");
-  let timeoutId = null;
+// document.addEventListener("DOMContentLoaded", function () {
+//   const dropdown = document.getElementsByClassName("modules-panel")[0];
+//   const modulesSelect = document.getElementById("modules-select");
+//   let timeoutId = null;
 
-  if (dropdown && modulesSelect) {
-    modulesSelect.addEventListener("mouseover", function () {
-      dropdown.style.display = "flex";
-    });
-    dropdown.addEventListener("mouseout", function () {
-      timeoutId = setTimeout(() => {
-        dropdown.style.display = "none";
-      }, 5000);
-    });
-  }
-});
+//   if (dropdown && modulesSelect) {
+//     modulesSelect.addEventListener("mouseover", function () {
+//       dropdown.style.display = "flex";
+//     });
+//     dropdown.addEventListener("mouseout", function () {
+//       timeoutId = setTimeout(() => {
+//         dropdown.style.display = "none";
+//       }, 5000);
+//     });
+//   }
+// });
