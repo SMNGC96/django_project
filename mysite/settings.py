c01719493aecc8d19b6f1b6afa17495abd19d46b
Sizwe <smngcemu@gmail.com>
2025-03-24 10:07:39 +0200

    Configure Database URL to Postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=600
+    )
 }
 
 
