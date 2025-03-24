5245f578e3e2dc148cdde0ac2212c103c64e0cdf
Sizwe <smngcemu@gmail.com>
2025-03-24 09:45:29 +0200

    Moved Django project files to correct root

--- django project/modules/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'modules'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<module_course_code>', views.detail, name='detail')
-]
